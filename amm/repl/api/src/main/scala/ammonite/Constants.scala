068f134c8a6e71fd6ade4a2084d4107d5229989b
Seth Tisue <seth@tisue.net>
2020-12-20 09:43:26 -0800

    add generated source file

---
+++ amm/repl/api/src/main/scala/ammonite/Constants.scala
@@ -0,0 +1,16 @@
+
+    package ammonite
+    object Constants{
+      val version = "2.3.8-3-05aed517"
+      val unstableVersion = "<fill-me-in-in-Constants.scala>"
+      val bspVersion = "2.0.0-M6"
+      val curlUrl = "<fill-me-in-in-Constants.scala>"
+      val unstableCurlUrl = "<fill-me-in-in-Constants.scala>"
+      val oldCurlUrls = Seq[(String, String)](
+        
+      )
+      val oldUnstableCurlUrls = Seq[(String, String)](
+        
+      )
+    }
+  
